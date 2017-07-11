ca6730084fcb6338a0d451fc615d81432854502f
zasmith <zach.13.smith@gmail.com>
2017-07-11 10:55:37 -0700

    Update positive_integer.rb

--- lib/puppet_x/puppetlabs/iis/property/positive_integer.rb
+++ lib/puppet_x/puppetlabs/iis/property/positive_integer.rb
@@ -8,7 +8,7 @@ module PuppetX
           end
           validate do |value|
             fail "#{self.name.to_s} should be an Integer" unless value.to_i.to_s == value.to_s
-            fail "#{self.name.to_s} should be greater than 0" unless value.to_i > 0
+            fail "#{self.name.to_s} should be greater than 0" unless value.to_i >= 0
           end
         end
       end
